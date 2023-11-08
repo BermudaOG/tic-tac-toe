a674f25261537bbdd7472918eeafac9bad2fc8c7
jose <torrj755@gmail.com>
2023-11-08 10:29:56 -0700

    y

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render } from '@testing-library/react';
+import Game from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the Tic-Tac-Toe game', () => {
+  const { getByText } = render(<Game />);
+  const playerXTurn = getByText(/Next Player: Player X/i);
+  expect(playerXTurn).toBeInTheDocument();
 });
